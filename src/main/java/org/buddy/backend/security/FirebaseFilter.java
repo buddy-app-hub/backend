a785cbeefbb4560915094e09d5278c59f9b0fb58
romeromaxi <mromerov@fi.uba.ar>
2024-06-27 09:36:28 -0300

    feat: add firebase token filter

---
+++ src/main/java/org/buddy/backend/security/FirebaseFilter.java
@@ -0,0 +1,42 @@
+package org.buddy.backend.security;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseToken;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class FirebaseFilter extends OncePerRequestFilter {
+    private static final int TOKEN_START = 7;
+
+    private void sendUnauthorizedResponse(HttpServletResponse response, String message) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, message);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+        if (header == null || !header.startsWith("Bearer ")) {
+            sendUnauthorizedResponse(response, "Missing or invalid Authorization header");
+            return;
+        }
+
+        String token = header.substring(TOKEN_START);
+        try {
+            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdTokenAsync(token).get();
+            // SecurityContextHolder.getContext().setAuthentication(authentication);
+            logger.info(decodedToken.getEmail());
+            filterChain.doFilter(request, response);
+        } catch (InterruptedException | ExecutionException e) {
+            SecurityContextHolder.clearContext();
+            sendUnauthorizedResponse(response, "Invalid token");
+        }
+    }
+}
---
+++ src/main/java/org/buddy/backend/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package org.buddy.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new FirebaseFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
