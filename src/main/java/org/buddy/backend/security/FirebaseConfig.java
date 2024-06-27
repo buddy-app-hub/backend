d48ce64d531cdb3ee9099bf896c0991499aa158b
romeromaxi <mromerov@fi.uba.ar>
2024-06-27 10:06:31 -0300

    fix: use firebase path in application.properties

--- src/main/java/org/buddy/backend/security/FirebaseConfig.java
+++ src/main/java/org/buddy/backend/security/FirebaseConfig.java
@@ -3,6 +3,7 @@ package org.buddy.backend.security;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +13,14 @@ import java.io.IOException;
 @Configuration
 public class FirebaseConfig {
 
+
+    @Value("${firebase.service-account.path}")
+    private String firebaseKeyPath;
+
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
         FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/firebaseKey.json");
+                new FileInputStream(firebaseKeyPath);
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
