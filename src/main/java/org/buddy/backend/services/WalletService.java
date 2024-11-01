e1587ac5685ce225d0795a176d19d647b39ec8cc
Agustin Gabrielli <agabrielli@fi.uba.ar>
2024-11-01 20:44:36 -0300

    Test to see if PAYMENTS_URL is causing issues

--- src/main/java/org/buddy/backend/services/WalletService.java
+++ src/main/java/org/buddy/backend/services/WalletService.java
@@ -5,26 +5,24 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-import java.util.Map;
-import java.util.List;
-
 @Service
 public class WalletService {
 
-    private final RestClient restClient;
+    // private final RestClient restClient;
 
     public WalletService(RestClient.Builder restClientBuilder, Environment environment) {
-        this.restClient = restClientBuilder
-                .baseUrl(environment.getProperty("PAYMENTS_URL", "http://host.docker.internal:8000"))
-                .build();
+        // this.restClient = restClientBuilder
+        //         .baseUrl(environment.getProperty("PAYMENTS_URL", "http://host.docker.internal:8000"))
+        //         .build();
     }
 
     public Wallet createWallet() {
-        return this.restClient.post()
-                .uri("/wallets")
-                .body(Map.of("transactions", List.of()))
-                .retrieve()
-                .body(Wallet.class);
+        return new Wallet();
+        // return this.restClient.post()
+        //         .uri("/wallets")
+        //         .body(Map.of("transactions", List.of()))
+        //         .retrieve()
+        //         .body(Wallet.class);
     }
 
 }
