79a07f13b707fb61dd2edd24e014f3fe36eebb05
Agustin Gabrielli <agabrielli@fi.uba.ar>
2024-11-01 21:19:14 -0300

    Restored wallet service

--- src/main/java/org/buddy/backend/services/WalletService.java
+++ src/main/java/org/buddy/backend/services/WalletService.java
@@ -1,5 +1,8 @@
 package org.buddy.backend.services;
 
+import java.util.List;
+import java.util.Map;
+
 import org.buddy.backend.models.Wallet;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
@@ -8,21 +11,20 @@ import org.springframework.web.client.RestClient;
 @Service
 public class WalletService {
 
-    // private final RestClient restClient;
+    private final RestClient restClient;
 
     public WalletService(RestClient.Builder restClientBuilder, Environment environment) {
-        // this.restClient = restClientBuilder
-        //         .baseUrl(environment.getProperty("PAYMENTS_URL", "http://host.docker.internal:8000"))
-        //         .build();
+        this.restClient = restClientBuilder
+                .baseUrl(environment.getProperty("PAYMENTS_URL", "http://host.docker.internal:8000"))
+                .build();
     }
 
     public Wallet createWallet() {
-        return new Wallet();
-        // return this.restClient.post()
-        //         .uri("/wallets")
-        //         .body(Map.of("transactions", List.of()))
-        //         .retrieve()
-        //         .body(Wallet.class);
+        return this.restClient.post()
+                .uri("/wallets")
+                .body(Map.of("transactions", List.of()))
+                .retrieve()
+                .body(Wallet.class);
     }
 
 }
